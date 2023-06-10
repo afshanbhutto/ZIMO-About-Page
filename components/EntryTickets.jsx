15d2e11cd2f23639927185892a06cfdc75174400
Afshan Bhutto <afshanbhutto6@gmail.com>
2023-06-10 20:12:26 +0500

    added other sections with full  responsivenessss

--- components/EntryTickets.jsx
+++ components/EntryTickets.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const EntryTickets = () => {
   return (
     <div className="container mx-auto ">
-      <div className=" absolute top-[1500px] md:top-[1600px] lg:mt-[40px] xl:mt-[700px]  flex items-center justify-center lg:justify-normal lg:gap-x-54 flex-col md:flex-row">
+      <div className=" absolute top-[1500px] md:top-[1600px] lg:mt-[40px] xl:mt-[500px]  flex items-center justify-center lg:justify-normal lg:gap-x-54 flex-col md:flex-row">
         <div className="text-left lg:flex-1 lg:justify-left justify-center items-center ">
           <img src="/zimo-logo.png" className='w-[200px] h-[50px] md:w-[200px] md:h-[60px] lg:w-[230px] lg:h-[90px]'  alt="" />
           <div className=" leading-2 mt-5 uppercase tracking-[4px] text-left mb-3">
@@ -16,7 +16,7 @@ const EntryTickets = () => {
         </div>
         <div className="lg:items-left justify-center items-center lg:justify-right lg:flex-2 float-right">
           <div
-            className="flex flex-col flex-2 items-center  w-[490px] h-[400px] lg:w-[600px] lg:h-[500px] xl:w-[700px] xl:h-[550px]"
+            className="flex flex-col flex-2 items-center  w-[490px] h-[400px] lg:w-[600px] lg:h-[500px] xl:w-[800px] xl:h-[600px]"
           >
             <img
               src="/65c8a1f0ee7329a44d1812b2f35ced5c.jpg"
